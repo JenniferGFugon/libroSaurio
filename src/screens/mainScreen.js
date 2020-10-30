76553a528344f6bd3407ff7321dfce50fc8b91f1
JenniferGFugon <jgurrerofugon500@gmail.com>
2020-10-30 15:50:17 -0600

    Agragar Hola a Pantalla principal

---
+++ src/screens/mainScreen.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+import react from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+  const mainScreen = ({ navigation }) => { 
+      return (
+        <View>
+            <Text>Hola</Text>
+        </View>
+      )
+   };
+
+export default mainScreen;
+    
