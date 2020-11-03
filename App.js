147ea5f3339ce00e7a92568f655f5536a21db7e4
KattiaBodden <kattiabodden@gmail.com>
2020-11-03 02:42:50 -0600

    Agregando la pantalla de búsqueda

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
- import mainScreen from "./src/screens/mainScreen";
+import mainScreen from "./src/screens/mainScreen";
 
- import { NavigationContainer} from '@react-navigation/native';
- import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import searchScreen from './src/screens/searchScreen';
 
- const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    
       <NavigationContainer>
         <Stack.Navigator initialRouteName="mainScreen" headerMode = 'none'>
-        <Stack.Screen name="mainScreen" component={mainScreen} />
+          <Stack.Screen name="mainScreen" component={mainScreen} />
+          <Stack.Screen name="searchScreen" component={searchScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     )
