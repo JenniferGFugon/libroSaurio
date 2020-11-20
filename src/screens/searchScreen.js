c9ad8e2ad8fbe2e0f64700c7761935dea1ac548f
KattiaBodden <kattiabodden@gmail.com>
2020-11-19 22:06:13 -0600

    Editando diseño de la pantalla de busqueda

--- src/screens/searchScreen.js
+++ src/screens/searchScreen.js
@@ -3,7 +3,7 @@ import react from "react";
 import { Container, Right, Content, Footer } from "native-base";
 import React, { useEffect, useState } from "react";
 
-import { StyleSheet, Text, View, Image, Dimensions,FlatList, ImageBackground } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions, FlatList, ImageBackground, Animated } from "react-native";
 import {Input, Title, Item, H1, Button, Header, Icon, Spinner, Card, CardItem, H3, Body, Left, Thumbnail} from "native-base";
 
 import backend from "../api/backend";
@@ -11,13 +11,15 @@ import getEnvVars from "../../enviroment";
 const { width, height } = Dimensions.get("window");
 const { apiUrl , apiImageUrl, apiImageSize } = getEnvVars();
 import { TouchableOpacity } from "react-native-gesture-handler";
-
+import { linear } from "react-native/Libraries/Animated/src/Easing";
+//import Animated from "react-native-reanimated";
+import { LinearGradient } from 'expo-linear-gradient';
 
 const searchScreen = ({ route, navigation }) => {
     const { search } = route.params;
     const [books, setBooks] = useState(null);
     const [error, setError] = useState(false);
-
+    const scrollX = React.useRef(new Animated.Value(0)).current;
     async function getSearchBooks() {
         try {
           const response = await backend.get(`${apiUrl}?keyword=${search}&lang=spanish`);  
@@ -27,12 +29,10 @@ const searchScreen = ({ route, navigation }) => {
         }
     }
 
-
     useEffect(() => {
         getSearchBooks();
     }, []);
 
-    
     // Verifica si el usuario ingresa información en el input de búsqueda
 
     if (!books) {
@@ -42,53 +42,74 @@ const searchScreen = ({ route, navigation }) => {
           </View>
         )
       }
-  
- 
-
  
     return (
         <Container>
-           
-                <ImageBackground style={styles.Fondo} source={require("../../assets/fondo.jpeg")}>
-                    <Text>
-                        <H1 style={styles.estiloH1}>Resultados de la Búsqueda</H1>
-                    </Text> 
-                    <FlatList
+           <Header style={styles.header} >
+              <Left> 
+                <Image source={require("../../assets/logo_computadora.png")} style={styles.logoImage} />
+              </Left>
+              <Image source={require("../../assets/logo_letras.png")} style={styles.letrasImage} />
+            </Header>
+              <LinearGradient colors={[colors= '#238723','#055e05','#000E21']} style={styles.LinearGradient}>
+                <Text style={styles.textHeader}>
+                  <H1 style={styles.estiloH1}>Resultados de la Búsqueda</H1>
+               </Text>
+                <Animated.FlatList
+                  showsHorizontalScrollIndicator={false}
+                  horizontal={true}
                   data={books}
                   keyExtractor={(item) => {
                     return item.ID;
-                  }}
+                  }}  
+                  contentContainerStyle={{alignItems:"center"}}
+                  //snapToInterval= {width * 0.72}
+                  decelerationRate={0}
+                  //bounces={false}
+                  onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                    { useNativeDriver: true }
+                  )}
+                  scrollEventThrottle={16}
                   ListEmptyComponent={<Text>¡No se han encontrado libros!</Text>}
-                  renderItem={({ item }) => {
+                  renderItem={({ item, index }) => {
+                    const inputRange = [
+                      (index - 1) * (width * 0.72),
+                      index  * (width * 0.72),
+                      (index + 1) * (width * 0.72),
+                    ];
+                    const translateY = scrollX.interpolate({
+                      inputRange,
+                      outputRange:[-30, -70, -30],
+                    });
                     return (
-                      <View >
+                      <View style={styles.firstView} >
                           <TouchableOpacity onPress={() => navigation.navigate("infoScreen", {ID: item.ID})}>
-                            <Card >
-                              <CardItem header bordered style={{backgroundColor: "#0f630f"}}>
-                                  <H3 style={styles.tituloLibro}>{item.title}</H3>
+                            <Animated.View style={styles.estiloView} style={{transform: [{translateY}] }}>
+                            <Card style={styles.estiloCard}>
+                              <CardItem header bordered style={styles.cardItem}>
+                                <H3 style={styles.tituloLibro}>{item.title}</H3>
                               </CardItem>                          
-                                <CardItem cardBody bordered>
-                                <Body>
+                               
+                                
                                   <Image  source={{ uri: `${item.cover}` }} style={styles.bookImage} />
-                              </Body>
-                                </CardItem> 
                               
-                                <CardItem footer bordered>
+                                
+                              
+                                <CardItem footer bordered style={styles.cardItem} >
                                   <Text style={styles.autoryPag} > {item.pages} Páginas</Text>
                                   <Right>
                                   <Text style={styles.autoryPag}> Autor: {item.author}</Text>
                                   </Right>
                               </CardItem>
-                            
-                            </Card>
+                              </Card>
+                            </Animated.View>
                         </TouchableOpacity>
-                    </View>
+                      </View>
                     )
                   }}
                 />
-                </ImageBackground>
-                
-         
+              </LinearGradient>
         </Container>
     );
 }
@@ -97,31 +118,111 @@ const styles = StyleSheet.create({
     Fondo: {
       width: width,
       height: height,
+      backgroundColor: 'green',
+      
+    },
+
+    LinearGradient: {
+      height: height,
+      width: width
+      
+    },
+
+    textHeader: {
+      fontFamily: "Times New Roman",
+      fontSize: 25,
+      fontWeight: "bold",
+      marginTop: 20,
+      marginBottom: 10,
+      textAlign: "center",
+      color: "#fff",
+      backgroundColor: "#173d17"
+    },
+
+    firstView: {
+      width: width * 0.80,
+      marginLeft: 25,
+      
+    },
+
+    estiloView: {
+      marginHorizontal: 10,
+      padding: 15,
+      alignItems: "center",
+      backgroundColor: "white",
+      borderRadius: 34,
+      width: width * 0.86,  
+      height: height * 0.70,  
+    },
+
+    estiloCard: {
+      marginHorizontal: 5,
+      paddingTop: 5,
+      alignItems: "center",
+      backgroundColor: "white",
+      borderRadius: 10,
+      width: width * 0.79, 
+      height: "auto",   
+      borderColor: "white",
+      borderStyle: "dotted",   
+      
     },
 
     estiloH1: {
-       color: '#f2f2f2',
-       textAlign: "center",
+      color: 'white',
+      textAlign: "center",
+      //paddingBottom: 30,
+      //backgroundColor: "#0f630f",
     },
 
     icono: {
         width: 19, 
     },
 
-    boton:{
-        backgroundColor: "green",
-        width: 50,
-        height: 40,
-        marginTop: 5,
-    },
-
     header: {
         backgroundColor: '#fff',
       },
-      bookImage: {
-        width: width * 0.99,
-        height: height * 0.75,
-      },
+
+    bookImage: {
+        width: width * 0.50,
+        height: height * 0.45,
+        marginLeft: 5,        
+    },
+    letrasImage: {
+        width: 180,
+        height: 30,
+        margin: 20,
+        marginRight: 50,
+    },
+  
+    logoImage: {
+        width: 50,
+        height: 33,
+        marginLeft: 10,
+    },
+
+    tituloLibro: {
+      fontFamily: "Times New Roman",
+      fontSize: 16,
+      fontWeight: "bold",
+      alignContent: "center",
+      color: "#000000",
+      
+    },
+
+    autoryPag: {
+      fontFamily: "Times New Roman",
+      fontSize: 18,
+      fontStyle: "italic"
+      //color:"fff",
+    },
+
+    cardItem: {
+      backgroundColor: "#49c46a",
+      borderWidth: 1,
+      borderColor: "white"
+    }
+
   });
 
 export default searchScreen;
